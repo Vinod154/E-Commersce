53cc3cb84fab4ed58eb853d0b0c4928e481a70a6
Vinod154 <gaikwadvinod154@gmail.com>
2020-08-13 14:34:22 +0530

    Added Product and Category Business Entity and created Database

---
+++ ClothBazar.Database/CBContact.cs
@@ -0,0 +1,22 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Database
+{
+   public class CBContact:DbContext
+    {
+        public CBContact():base("ClothBazarConnection")
+        {
+
+        }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> categories { get; set; }
+
+    }
+}
